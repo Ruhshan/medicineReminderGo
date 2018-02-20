9552e75008006ad8f9d3de06e05ef0af970b842b
Ruhshan <ruhshan.ahmed@gmail.com>
2018-02-20 18:02:02 +0600

    post and get enabled

--- main.go
+++ main.go
@@ -18,8 +18,37 @@ type Medicine struct {
 	Remaining string `json:"Remaining"`
 }
 
+func createMedicines(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+
+	var NewMed Medicine
+
+	err := decoder.Decode(&NewMed)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer r.Body.Close()
+
+	log.Println(NewMed)
+
+	db, err := sql.Open("sqlite3", "./medicieDB.db")
+	defer db.Close()
+
+	insertCmd := fmt.Sprintf(`insert into medicines(name, dose, remaining) values("%s", %s, %s)`, NewMed.Name, NewMed.Dose, NewMed.Remaining)
+
+	_, err = db.Exec(insertCmd)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
 func retriveMedicines(w http.ResponseWriter, r *http.Request) {
 	db, err := sql.Open("sqlite3", "./medicieDB.db")
+	defer db.Close()
 	rows, err := db.Query("select name, dose, remaining from medicines")
 	if err != nil {
 		log.Fatal(err)
@@ -49,7 +78,8 @@ func retriveMedicines(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/medicines", retriveMedicines)
+	myRouter.HandleFunc("/medicines", retriveMedicines).Methods("GET")
+	myRouter.HandleFunc("/medicines", createMedicines).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
